decfe602efacf3702ca17644ae474e7217e1a09b
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-01-23 19:22:21 +0530

    hmm

--- mail.php
+++ mail.php
@@ -24,19 +24,19 @@
   $name = $_POST['name'];
 $email= $_POST['email_id'];
  $phone = $_POST['phone'];
-$subject_mail = $_POST['subject'];
+$mail = $_POST['subject'];
 $message = $_POST['message'];
 $subject = "Mail from fatima_school_website contact form";
 $txt = "You have recieved an email from ".$name ."\r\nEmail Id: " .$email ."\r\nPhone No: ". $phone ."\r\n Subject: " .$subject_mail ."\r\n Message:" .$message;
 
-   if(empty($name) || empty($name) || empty($subject_mail) || empty($message))
+   if(empty($name) || empty($name) || empty($mail) || empty($message))
     {
         header('location:error-404.html');
     }
     else
    {
        $to = "nurajabegum81@gmail.com";
-        if(mail($to,$subject_mail,$message,$email,$txt, $subject))
+        if(mail($to,$mail,$message,$email,$txt, $subject))
        {
            header("location:thankyou.html");
        }
