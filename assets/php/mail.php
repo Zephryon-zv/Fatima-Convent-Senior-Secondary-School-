87a97600f522e95cec6c764dac2af4c2e7ecbaba
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-01-23 18:56:00 +0530

    Update

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,49 +1,51 @@
 <?php
-$name = $_POST['name'];
+// $name = $_POST['name'];
+// $email= $_POST['email_id'];
+// $phone = $_POST['phone'];
+// $subject_mail = $_POST['subject'];
+// $message = $_POST['message'];
+// $subject = "Mail from fatima_school_website contact form";
+// $to = "nurajabegum81@gmail.com"
+// $headers = "From: ".$name. "\r\n" .
+
+// $txt = "You have recieved an email from ".$name ."\r\nEmail Id: " .$email ."\r\nPhone No: ". $phone ."\r\n Subject: " .$subject_mail ."\r\n Message:" .$message;
+
+// if($email! = NULL){
+//     mail($to, $subject, $txt, $headers);
+// }
+
+
+
+
+
+
+ if(isset($_POST['btn-send']))
+ {
+  $name = $_POST['name'];
 $email= $_POST['email_id'];
-$phone = $_POST['phone'];
+ $phone = $_POST['phone'];
 $subject_mail = $_POST['subject'];
 $message = $_POST['message'];
 $subject = "Mail from fatima_school_website contact form";
-$to = "nurajabegum81@gmail.com"
-$headers = "From: ".$name. "\r\n" .
-
 $txt = "You have recieved an email from ".$name ."\r\nEmail Id: " .$email ."\r\nPhone No: ". $phone ."\r\n Subject: " .$subject_mail ."\r\n Message:" .$message;
 
-if($email! = NULL){
-    mail($to, $subject, $txt, $headers);
+   if(empty($name) || empty($name) || empty($subject) || empty($message))
+    {
+        header('location:index.php?error');
+    }
+    else
+   {
+       $to = "nurajabegum81@gmail.com";
+        if(mail($to,$subject_mail,$message,$email,$txt, $subject))
+       {
+           header("location:index.php?success");
+       }
+    }
+ }
+ else
+ {
+    header("location:index.php");
 }
-
-
-
-
-
-
-// if(isset($_POST['btn-send']))
-// {
-//    $UserName = $_POST['UName'];
-//    $Email = $_POST['Email'];
-//    $Subject = $_POST['Subject'];
-//    $Msg = $_POST['msg'];
-
-//    if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
-//    {
-//        header('location:index.php?error');
-//    }
-//    else
-//    {
-//        $to = "admin@onlineittuts.com";
-
-//        if(mail($to,$Subject,$Msg,$Email))
-//        {
-//            header("location:index.php?success");
-//        }
-//    }
-// }
-// else
-// {
-//     header("location:index.php");
-// }
 
 
 
